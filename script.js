1b9870ff6289bd4b2e922d3d295292b9ae4df4bd
paulinaboro <paulina.borowiecka554@gmail.com>
2019-04-03 15:55:26 +0200

    Form flow is showing specific steps, changed landing page

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 
 // Variables for the progress bar to change color when clicking "continue"
 const continueBtn1 = document.querySelector("[data-field=continueBtn1]");
@@ -9,10 +9,10 @@ const progressInfo = document.querySelector("[data-field=progress]");
 const progressPay = document.querySelector("[data-field=progress1]");
 
 // variables for changing the form to the next fieldset
-const fieldset1 = document.querySelector("[data-field=fieldset2]");
-const fieldset2 = document.querySelector("[data-field=fieldset2]");
-const fieldset3 = document.querySelector("[data-field=fieldset3]");
-const fieldset4 = document.querySelector("[data-field=fieldset4]");
+const fieldset1 = document.querySelector("#mainForm #fieldset1");
+const fieldset2 = document.querySelector("#mainForm #fieldset2");
+const fieldset3 = document.querySelector("#mainForm #fieldset3");
+const fieldset4 = document.querySelector("#mainForm #fieldset4");
 
 // variables for showing the second product on the product page, after choosing the delivery form
 const deliveryChoice1 = document.querySelector("[data-field=deliveryChoice1]");
@@ -21,30 +21,53 @@ const secondProduct = document.querySelector("[data-field=secondProduct]");
 const secondProduct2 = document.querySelector("[data-field=secondProduct2]");
 
 // Functions for the progress bar to change color when clicking "continue"
-continueBtn1.addEventListener("click", animateProgressBar);
-continueBtn2.addEventListener("click", animateProgressBar);
-continueBtn3.addEventListener("click", animateProgressBar);
+continueBtn1.addEventListener("click", switchFieldset);
+continueBtn2.addEventListener("click", switchFieldset);
+continueBtn3.addEventListener("click", switchFieldset);
 
-function animateProgressBar(event){
-  if(event.target === continueBtn1){
-  progressInfo.classList.add("active");
-  //fieldset1.classList.add("hidden");  not working yet
-  console.log("working!");
-}else if(event.target === continueBtn2){
-  progressPay.classList.add("active");
-  
-}};
+// Using switch statement to change the fieldsets and showing the progress on the info bar
+function switchFieldset(event) {
+  switch (event.target) {
+    case continueBtn1:
+      progressInfo.classList.add("active");
+      fieldset1.classList.add("hidden");
+      fieldset2.classList.remove("hidden");
+      break;
+    case continueBtn2:
+      progressPay.classList.add("active");
+      fieldset2.classList.add("hidden");
+      fieldset3.classList.remove("hidden");
+      break;
+    case continueBtn3:
+      fieldset3.classList.add("hidden");
+      fieldset4.classList.remove("hidden");
+      break;
+    default:
+      console.log("error");
+  }
+}
 
-// Function for showing the second product when choosing the delivery 
+// Function for showing the second product when choosing the delivery
 deliveryChoice1.addEventListener("click", showProduct);
 deliveryChoice2.addEventListener("click", showProduct);
 
-function showProduct(event){
-  if(event.target === deliveryChoice1){
-  secondProduct.classList.remove("hidden");
-}else if(event.target === deliveryChoice2){
-  secondProduct2.classList.remove("hidden");
-}};
+function showProduct(event) {
+  if (event.target === deliveryChoice1) {
+    secondProduct.classList.toggle("hidden");
+  } else if (event.target === deliveryChoice2) {
+    secondProduct2.classList.toggle("hidden");
+  }
+}
+
+// Show the next fieldset
+// function nextFieldset(event){
+
+//   if (event.target === continueBtn1) {
+//   fieldset1.classList.add("hidden");
+// }
+// document.continueBtn1.addEventListener("click", function() {
+//   continueBtn1.classList.add("hidden");
+// });
 
 // Alert first that this is a school project
 /*
